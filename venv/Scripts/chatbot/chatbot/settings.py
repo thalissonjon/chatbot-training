4a7023d50e097d6030aefeae5fa925ad17156bf7
Thalisson <thalissonjon@gmail.com>
2023-09-11 17:39:33 -0300

    Alterações no models
    - Verificar integração do models com o views (pegar fala atual e dar outras escolhas, mexer no models tambem)
    - Ver como fazer o botao de retorno
    - Scripted chatbot (pesquisar)
    - Testar fluxo de mensagens

--- venv/Scripts/chatbot/chatbot/settings.py
+++ venv/Scripts/chatbot/chatbot/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chatbot_app.apps.ChatbotAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- venv/Scripts/chatbot/chatbot_app/models.py
+++ venv/Scripts/chatbot/chatbot_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
+
+
 # Create your models here.
+class userMsg(models.Model):
+    userText = models.TextField()
+
+    def __str__(self):
+        return self.userText
+    
+class botMsg(models.Model):
+    CONSULTAS = (
+        ('1', 'Verificar consultas'),
+        ('2,', 'Agendar consultas'),
+        ('3', 'Retornar')
+    )
+
+    choiceInt = models.PositiveSmallIntegerField(
+        choices = CONSULTAS,
+        default=3
+    )
+
+    botResp = models.ForeignKey(userMsg, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=100)
+    pos = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+    
+
+    # choice = models.PositiveSmallIntegerField(
+    #     choices = Consultas.choices
+    # )
