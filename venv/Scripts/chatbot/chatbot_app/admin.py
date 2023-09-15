7a163d81769b79ced87ef8c0f15a8d1d6286670a
Thalisson <thalissonjon@gmail.com>
2023-09-15 16:49:53 -0300

    att

--- venv/Scripts/chatbot/chatbot_app/admin.py
+++ venv/Scripts/chatbot/chatbot_app/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-class optionsAdmin(admin.ModelAdmin):
-    list_display = ['options']
-admin.site.register(userMsg, optionsAdmin)
+# class optionsAdmin(admin.ModelAdmin):
+#     list_display = ['options']
+# admin.site.register(userMsg, optionsAdmin)
 
-class respAdmin(admin.ModelAdmin):
-    list_display = ['msg', 'resp']
-admin.site.register(botMsg, respAdmin)
+# class respAdmin(admin.ModelAdmin):
+#     list_display = ['msg', 'resp']
+# admin.site.register(botMsg, respAdmin)
--- venv/Scripts/chatbot/chatbot_app/urls.py
+++ venv/Scripts/chatbot/chatbot_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('chat/', views.botAnswer, name='chat')
+    path('chat/', views.chat_view, name='chat')
 ]
